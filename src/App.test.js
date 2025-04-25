c2d2cfbe025289c0564b93c6c03b2587a56232da
Jerod Brackin <jbrackin1@gmail.com>
2025-04-25 02:31:50 -0500

    test:\ render each test seperately per component

--- src/App.test.js
+++ src/App.test.js
@@ -27,12 +27,45 @@ test('renders HomePage, MarketPlacePage, and About page correctly', async () => 
     </MemoryRouter>
   );
 
-  // HomePage route
-  expect(screen.getByText(/Find the best insurance plans tailored to your needs./i)).toBeInTheDocument();
-
-  // Navigate to MarketPlacePage route
-  expect(screen.queryByText(/MarketPlacePage/i)).toBeInTheDocument();
-
-  // Navigate to About route
-  expect(screen.queryByText(/About Us/i)).toBeInTheDocument();
+  test('renders HomePage correctly', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider theme={mockTheme}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </ThemeProvider>
+      </MemoryRouter>
+    );
+  
+    expect(screen.getByText(/Find the best insurance plans tailored to your needs./i)).toBeInTheDocument();
+  });
+  
+  test('renders MarketPlacePage correctly', () => {
+    render(
+      <MemoryRouter initialEntries={['/marketplace']}>
+        <ThemeProvider theme={mockTheme}>
+          <Routes>
+            <Route path="/marketplace" element={<MarketPlacePage />} />
+          </Routes>
+        </ThemeProvider>
+      </MemoryRouter>
+    );
+  
+    expect(screen.queryByText(/MarketPlacePage/i)).toBeInTheDocument();
+  });
+  
+  test('renders About page correctly', () => {
+    render(
+      <MemoryRouter initialEntries={['/about']}>
+        <ThemeProvider theme={mockTheme}>
+          <Routes>
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </ThemeProvider>
+      </MemoryRouter>
+    );
+  
+    expect(screen.queryByText(/About Us/i)).toBeInTheDocument();
+  });
 });
