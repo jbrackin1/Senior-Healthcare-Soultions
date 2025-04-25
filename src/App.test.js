b528e2a418d32c1cd2f4d58762aac6cba2c817e6
Jerod Brackin <jbrackin1@gmail.com>
2025-04-25 02:03:14 -0500

    fix\Another App.test.js fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import MarketPlacePage from './pages/Insurance/MarketplaceAPIPage/MarketPlacePage' // Adjust path as needed
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/senior healthcare solutions/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders form inputs and title', () => {
+  render(
+    <MemoryRouter>
+      <MarketPlacePage />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText(/Customize Your Preferences/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/Household Income/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/Age/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/Gender/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/Zip Code/i)).toBeInTheDocument();
 });
