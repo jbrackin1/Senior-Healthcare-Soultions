890a967fb5ff5fae2b10d8c36c279eda44d730b8
Jerod Brackin <jbrackin1@gmail.com>
2025-05-09 19:14:28 -0500

    fix: ci.yml fix 2

--- src/App.js
+++ src/App.js
@@ -1,57 +1,41 @@
 /** @format */
 
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
-import { lightTheme, darkTheme } from "./styles/themes/LightDarkTheme";
+import { darkTheme, lightTheme } from "./styles/themes/LightDarkTheme";
 
 // Components
 import AdBanner from "./components/ui/Feedback/Ads/AdBanner";
-import TopBannerComponent from "./components/ui/Global/layout/TopBanner";
 import BottomFooter from "./components/ui/Global/layout/BottomFooter";
+import TopBannerComponent from "./components/ui/Global/layout/TopBanner";
 
 // Pages and Components
-import SignUp from "./pages/Generic/SignUp";
-import MarketPlacePage from "./pages/Insurance/MarketplaceAPIPage/MarketPlacePage";
-import HomePage from "./pages/Generic/HomePage";
-import About from "./pages/Generic/About";
-import Careers from "./pages/Generic/Careers";
-import Contact from "./pages/Generic/Contact";
-import LoginSignup from "./pages/Users/Signup";
-import Dash from "./pages/Dash/Dash";
-import PrivacyPolicy from "./pages/Legal/PrivacyPolicy";
-import TermsOfService from "./pages/Legal/TermsOfService";
-import Testimonials from "./pages/Generic/Testimonials";
-import Profile from "./pages/Users/UserProfile";
-import Resources from "./pages/Generic/Resources";
-import PractitionerPage from "./pages/Users/Practitioner";
-import Blog from "./pages/Generic/Blog";
 import DrugCoverage from "./components/ui/Global/data-display/DrugCoverage";
 import VADrugCoverage from "./components/ui/Global/data-display/VADrugCoverage";
+import About from "./pages/Generic/About";
+import Blog from "./pages/Generic/Blog";
+import Careers from "./pages/Generic/Careers";
+import Contact from "./pages/Generic/Contact";
+import HomePage from "./pages/Generic/HomePage";
+import Resources from "./pages/Generic/Resources";
+import SignUp from "./pages/Generic/SignUp";
+import Testimonials from "./pages/Generic/Testimonials";
+import MarketPlacePage from "./pages/Insurance/MarketplaceAPIPage/MarketPlacePage";
+import PrivacyPolicy from "./pages/Legal/PrivacyPolicy";
+import TermsOfService from "./pages/Legal/TermsOfService";
+import LoginSignup from "./pages/Users/Signup";
+import Profile from "./pages/Users/UserProfile";
 // import ComparePlans from "./pages/Insurance/Compare";
 import PlanDetail from "./components/ui/Plan/PlanDetail";
 
 // Context and Services
-import { AuthProvider } from "./services/hooks/AuthProvider";
-import { PlanProvider } from "./services/hooks/PlanContext";
-import { getCignaToken } from "./services/Api/Cigna/getCignaToken";
 import PageWrapper from "./components/ui/Global/layout/PageWrapper";
+import { AuthProvider } from "./services/hooks/AuthProvider";
 
 function App() {
 	const [isDarkMode, setIsDarkMode] = useState(false);
-
-	// useEffect(() => {
-	// 	const fetchToken = async () => {
-	// 		try {
-	// 			const token = await getCignaToken();
-	// 			console.log("Cigna Token:", token);
-	// 		} catch (error) {
-	// 			console.error("Failed to fetch token:", error);
-	// 		}
-	// 	};
-	// 	fetchToken();
-	// }, []);
 
 	return (
 		<AuthProvider>
